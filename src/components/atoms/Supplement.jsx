dcb9c61126deca5e4f105dc89c25792a1e74e950
ryo-fujinone <117496712+ryo-fujinone@users.noreply.github.com>
2023-11-25 11:09:45 +0900

    Fixed an unknown prop warning in styled-components

--- src/components/atoms/Supplement.jsx
+++ src/components/atoms/Supplement.jsx
@@ -3,12 +3,12 @@ import styled from "styled-components";
 
 const StyledSpan = styled.span`
     display: block;
-    margin-left: ${(props) => props.marginLeft || 0};
+    margin-left: ${(props) => props.$marginLeft || 0};
 `;
 
 const Supplement = ({ children, marginLeft = "20px" }) => {
     return (
-        <StyledSpan className="supplement" marginLeft={marginLeft}>
+        <StyledSpan className="supplement" $marginLeft={marginLeft}>
             {children}
         </StyledSpan>
     );
